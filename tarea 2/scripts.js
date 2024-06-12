4bd406e4052b350f26978f8019b70b9c638b3dc7
yudithkleon <yudith.leon@agileinnova.org>
2024-06-12 16:20:39 -0500

    cambios en carpetas

---
+++ tarea 2/scripts.js
@@ -0,0 +1,8 @@
+// =====================Sumar 2 números=================================//
+
+let num1 = 10;
+let num2 = 5;
+
+const sumar = num1 + num2;
+
+console.log(`La suma de ${num1} + ${num2}es: `, sumar);
--- tarea1/scripts.js
+++ tarea1/scripts.js
@@ -9,12 +9,3 @@ const prom = (n1 + n2 + n3) / 3;
 
 //Salida
 console.log(`El promedio de las notas ${n1}, ${n2}, ${n3} es:`, prom, "Pts");
-
-// =====================Sumar 2 números=================================//
-
-let num1 = 10;
-let num2 = 5;
-
-const sumar = num1 + num2;
-
-console.log(`La suma de ${num1} + ${num2}es: `, sumar);
