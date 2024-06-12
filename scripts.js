8dab421e95f9bf628ddaa44881a147c3c9d1c887
yudithkleon <yudith.leon@agileinnova.org>
2024-06-12 16:04:06 -0500

    feat/ algoritmo de promedios

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Datos
+let n1 = 3.5
+let n2 = 5
+let n3= 3
+//let prom =0
+
+//Procedimiento
+const prom = (n1 + n2 + n3)/3
+
+//Salida
+console.log(`El promedio de las notas ${n1}, ${n2}, ${n3} es:`, prom, "Pts")
