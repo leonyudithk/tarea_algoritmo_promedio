f0a067c0ff0b8236890e326fbe63b07982520001
yudithkleon <yudith.leon@agileinnova.org>
2024-06-12 16:13:49 -0500

    tarea 2- suma de números

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 // Datos
-let n1 = 3.5
-let n2 = 5
-let n3= 3
+let n1 = 3.5;
+let n2 = 5;
+let n3 = 3;
 //let prom =0
 
 //Procedimiento
-const prom = (n1 + n2 + n3)/3
+const prom = (n1 + n2 + n3) / 3;
 
 //Salida
-console.log(`El promedio de las notas ${n1}, ${n2}, ${n3} es:`, prom, "Pts")
+console.log(`El promedio de las notas ${n1}, ${n2}, ${n3} es:`, prom, "Pts");
+
+// =====================Sumar 2 números=================================//
+
+let num1 = 10;
+let num2 = 5;
+
+const sumar = num1 + num2;
+
+console.log(`La suma de ${num1} + ${num2}es: `, sumar);
